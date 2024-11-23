cfd7174b1ee039337cc65769fe89775ea396cc3a
johnny-jyj <140963841+johnny-jyj@users.noreply.github.com>
2024-11-23 21:20:09 +0800

    Update mosdns_config_gen.sh

--- mosdns_config_gen.sh
+++ mosdns_config_gen.sh
@@ -7,13 +7,13 @@ wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/ge
 cd ..
 ls -a
 
-./v2dat unpack mosdns_config/geoip -o mosdns_config/mosdns_ip -f us geoip.dat
-./v2dat unpack mosdns_config/geoip -o mosdns_config/mosdns_ip -f ca geoip.dat
-./v2dat unpack mosdns_config/geoip -o mosdns_config/mosdns_ip -f jp geoip.dat
-./v2dat unpack mosdns_config/geoip -o mosdns_config/mosdns_ip -f hk geoip.dat
-./v2dat unpack mosdns_config/geoip -o mosdns_config/mosdns_ip -f sg geoip.dat
-./v2dat unpack mosdns_config/geoip -o mosdns_config/mosdns_ip -f cn geoip.dat
-./v2dat unpack mosdns_config/geoip -o mosdns_config/mosdns_ip -f uk geoip.dat
+./v2dat unpack geoip -o mosdns_config/mosdns_ip -f us mosdns_config/geoip.dat
+./v2dat unpack geoip -o mosdns_config/mosdns_ip -f ca mosdns_config/geoip.dat
+./v2dat unpack geoip -o mosdns_config/mosdns_ip -f jp mosdns_config/geoip.dat
+./v2dat unpack geoip -o mosdns_config/mosdns_ip -f hk mosdns_config/geoip.dat
+./v2dat unpack geoip -o mosdns_config/mosdns_ip -f sg mosdns_config/geoip.dat
+./v2dat unpack geoip -o mosdns_config/mosdns_ip -f cn mosdns_config/geoip.dat
+./v2dat unpack geoip -o mosdns_config/mosdns_ip -f uk mosdns_config/geoip.dat
 
-./v2dat unpack mosdns_config/geosite  -o mosdns_config/mosdns_site -f cn geosite.dat
-./v2dat unpack mosdns_config/geosite  -o mosdns_config/mosdns_site -f category-games@cn geosite.dat
+./v2dat unpack geosite  -o mosdns_config/mosdns_site -f cn mosdns_config/geosite.dat
+./v2dat unpack geosite  -o mosdns_config/mosdns_site -f category-games@cn mosdns_config/geosite.dat
