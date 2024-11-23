f77b4de9bd18fb74214b2eb9496084a95d37b657
johnny-jyj <140963841+johnny-jyj@users.noreply.github.com>
2024-11-23 17:51:11 +0800

    Update mosdns_config_gen.sh

--- mosdns_config_gen.sh
+++ mosdns_config_gen.sh
@@ -2,17 +2,17 @@ mkdir mosdns_config
 cd mosdns_config
 mkdir mosdns_ip
 mkdir mosdns_site
-cd ..
 wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
 wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
+cd ..
 
-./v2dat unpack geoip -o mosdns_config/mosdns_ip -f us geoip.dat
-./v2dat unpack geoip -o mosdns_config/mosdns_ip -f ca geoip.dat
-./v2dat unpack geoip -o mosdns_config/mosdns_ip -f jp geoip.dat
-./v2dat unpack geoip -o mosdns_config/mosdns_ip -f hk geoip.dat
-./v2dat unpack geoip -o mosdns_config/mosdns_ip -f sg geoip.dat
-./v2dat unpack geoip -o mosdns_config/mosdns_ip -f cn geoip.dat
-./v2dat unpack geoip -o mosdns_config/mosdns_ip -f uk geoip.dat
+github.com/urlesistiana/v2dat/v2data unpack mosdns_config/geoip -o mosdns_config/mosdns_ip -f us geoip.dat
+github.com/urlesistiana/v2dat/v2data unpack mosdns_config/geoip -o mosdns_config/mosdns_ip -f ca geoip.dat
+github.com/urlesistiana/v2dat/v2data unpack mosdns_config/geoip -o mosdns_config/mosdns_ip -f jp geoip.dat
+github.com/urlesistiana/v2dat/v2data unpack mosdns_config/geoip -o mosdns_config/mosdns_ip -f hk geoip.dat
+github.com/urlesistiana/v2dat/v2data unpack mosdns_config/geoip -o mosdns_config/mosdns_ip -f sg geoip.dat
+github.com/urlesistiana/v2dat/v2data unpack mosdns_config/geoip -o mosdns_config/mosdns_ip -f cn geoip.dat
+github.com/urlesistiana/v2dat/v2data unpack mosdns_config/geoip -o mosdns_config/mosdns_ip -f uk geoip.dat
 
-./v2dat unpack geosite  -o mosdns_config/mosdns_site -f cn geosite.dat
-./v2dat unpack geosite  -o mosdns_config/mosdns_site -f category-games@cn geosite.dat
+github.com/urlesistiana/v2dat/v2data unpack mosdns_config/geosite  -o mosdns_config/mosdns_site -f cn geosite.dat
+github.com/urlesistiana/v2dat/v2data unpack mosdns_config/geosite  -o mosdns_config/mosdns_site -f category-games@cn geosite.dat
