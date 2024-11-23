38cf47ae80c196aa9877b5430b232653295c311c
johnny-jyj <140963841+johnny-jyj@users.noreply.github.com>
2024-11-23 18:38:06 +0800

    Update mosdns_config_gen.sh

--- mosdns_config_gen.sh
+++ mosdns_config_gen.sh
@@ -5,14 +5,15 @@ mkdir mosdns_site
 wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
 wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
 cd ..
+ls -a
 
-github.com/urlesistiana/v2dat/v2data unpack mosdns_config/geoip -o mosdns_config/mosdns_ip -f us geoip.dat
-github.com/urlesistiana/v2dat/v2data unpack mosdns_config/geoip -o mosdns_config/mosdns_ip -f ca geoip.dat
-github.com/urlesistiana/v2dat/v2data unpack mosdns_config/geoip -o mosdns_config/mosdns_ip -f jp geoip.dat
-github.com/urlesistiana/v2dat/v2data unpack mosdns_config/geoip -o mosdns_config/mosdns_ip -f hk geoip.dat
-github.com/urlesistiana/v2dat/v2data unpack mosdns_config/geoip -o mosdns_config/mosdns_ip -f sg geoip.dat
-github.com/urlesistiana/v2dat/v2data unpack mosdns_config/geoip -o mosdns_config/mosdns_ip -f cn geoip.dat
-github.com/urlesistiana/v2dat/v2data unpack mosdns_config/geoip -o mosdns_config/mosdns_ip -f uk geoip.dat
+v2data unpack mosdns_config/geoip -o mosdns_config/mosdns_ip -f us geoip.dat
+v2data unpack mosdns_config/geoip -o mosdns_config/mosdns_ip -f ca geoip.dat
+v2data unpack mosdns_config/geoip -o mosdns_config/mosdns_ip -f jp geoip.dat
+v2data unpack mosdns_config/geoip -o mosdns_config/mosdns_ip -f hk geoip.dat
+v2data unpack mosdns_config/geoip -o mosdns_config/mosdns_ip -f sg geoip.dat
+v2data unpack mosdns_config/geoip -o mosdns_config/mosdns_ip -f cn geoip.dat
+v2data unpack mosdns_config/geoip -o mosdns_config/mosdns_ip -f uk geoip.dat
 
-github.com/urlesistiana/v2dat/v2data unpack mosdns_config/geosite  -o mosdns_config/mosdns_site -f cn geosite.dat
-github.com/urlesistiana/v2dat/v2data unpack mosdns_config/geosite  -o mosdns_config/mosdns_site -f category-games@cn geosite.dat
+v2data unpack mosdns_config/geosite  -o mosdns_config/mosdns_site -f cn geosite.dat
+v2data unpack mosdns_config/geosite  -o mosdns_config/mosdns_site -f category-games@cn geosite.dat
