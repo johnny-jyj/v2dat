b8b726d0fea48b1ae7c5ed319494855b5b8820bd
johnny-jyj <140963841+johnny-jyj@users.noreply.github.com>
2025-04-06 16:41:59 +0800

    Update mosdns_config_gen.sh

--- mosdns_config_gen.sh
+++ mosdns_config_gen.sh
@@ -28,6 +28,7 @@ cd ..
 ./v2dat unpack geosite  -o mosdns_config/mosdns_site -f category-games@cn mosdns_config/geosite.dat
 ./v2dat unpack geosite  -o mosdns_config/mosdns_site -f category-games mosdns_config/geosite.dat
 ./v2dat unpack geosite  -o mosdns_config/mosdns_site -f apple-cn mosdns_config/geosite.dat
+./v2dat unpack geosite  -o mosdns_config/mosdns_site -f tld-cn mosdns_config/geosite.dat
 ./v2dat unpack geosite  -o mosdns_config/mosdns_site -f icloud mosdns_config/geosite.dat
 ./v2dat unpack geosite  -o mosdns_config/mosdns_site -f github mosdns_config/geosite.dat
 ./v2dat unpack geosite  -o mosdns_config/mosdns_site -f google mosdns_config/geosite.dat
